04ebc009ef3a6831a2d9515e65d539ca950ce761
hughjazzman <wira.azmoon@gmail.com>
2020-10-02 13:25:07 +0800

    Update GPIO setPin

--- Lab/myRTX/GPIO.h
+++ Lab/myRTX/GPIO.h
@@ -4,10 +4,14 @@
 #include "MKL25Z4.h"                    // Device header
 #include "basic.h"
 
+typedef enum {LOW, HIGH} value_t;
+
 void initGPIOPort(PORT_Type *port, uint8_t pin);
 
 GPIO_Type* getGPIO(PORT_Type* port);
 
 void InitGPIOAll(PORT_Type *port, uint8_t pins[], uint8_t numPins);
+
+void setPin(PORT_Type *port, uint8_t pin, value_t value);
 
 #endif
